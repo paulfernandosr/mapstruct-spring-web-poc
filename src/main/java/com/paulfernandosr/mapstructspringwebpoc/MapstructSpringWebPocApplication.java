81ab266f829f8f84d7a8a451b275d81ab801b53f
Paul Fernando Sacaca Ramirez <paulfernandosr@gmail.com>
2024-09-04 02:37:03 -0500

    initialize project

---
+++ src/main/java/com/paulfernandosr/mapstructspringwebpoc/MapstructSpringWebPocApplication.java
@@ -0,0 +1,13 @@
+package com.paulfernandosr.mapstructspringwebpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructSpringWebPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructSpringWebPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulfernandosr/mapstructspringwebpoc/MapstructSpringWebPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulfernandosr.mapstructspringwebpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructSpringWebPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
